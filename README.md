b7c6c286ec924c999edfb81769eb661f067afa22
lamarcaa <g.lamarca@outlook.com>
2023-10-10 10:03:30 -0300

    comeco do filtro

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ flutter pub add flutter_localizations --sdk=flutter
 flutter pub add intl:any
 
 limpar projeto ao dar pull: flutter pub get --no-example
+
+buildar no celular pelo wifi:
+
+adb tcpip [ip], adb connect ip:[porta]
+
+caminho adb: C:\Users\glama\AppData\Local\Android\Sdk\platform-tools
--- lib/users/treinador/analiseComparativa.dart
+++ lib/users/treinador/analiseComparativa.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:unasplash/componentes/cardTreinoAvaliativo.dart';
 import 'package:unasplash/componentes/titulo.dart';
 
+enum ExerciseFilter { Craw }
+
 void main() {
   runApp(MaterialApp(
     home: AnaliseComparativa(),
@@ -9,77 +11,111 @@ void main() {
 }
 
 class AnaliseComparativa extends StatefulWidget {
-  const AnaliseComparativa({super.key});
+  const AnaliseComparativa({Key? key}) : super(key: key);
 
   @override
   State<AnaliseComparativa> createState() => _AnaliseComparativaState();
 }
 
 class _AnaliseComparativaState extends State<AnaliseComparativa> {
+  Set<ExerciseFilter> filters = <ExerciseFilter>{};
+  TextEditingController busca = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Titulo(
-                titulo: 'ANÁLISE COMPARATIVA',
-                subTitulo:
-                    'Filtre, pesquise e compare o desempenho dos atletas!'),
-            CardTreino(
-              nomeUsuario: 'Gabriel Lamarca Galdino da Silva',
-              dataTreino: 'Realizado em: 03/10 - Quarta Feira',
-              Modalidade: 'Craw',
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Titulo(
+            titulo: 'ANÁLISE COMPARATIVA',
+            subTitulo: 'Filtre, pesquise e compare o desempenho dos atletas!',
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextField(
+              controller: busca,
+              decoration: InputDecoration(
+                labelText: 'Nome do atleta',
+                prefixIcon: Icon(Icons.search),
+              ),
+              onSubmitted: buscarAtleta,
             ),
-            CardTreino(
-              nomeUsuario: 'Ana Maria',
-              dataTreino: 'Realizado em: 04/10 - Quinta Feira',
-              Modalidade: 'Costas',
-            ),
-            CardTreino(
-              nomeUsuario: 'João da Silva',
-              dataTreino: 'Realizado em: 05/10 - Sexta Feira',
-              Modalidade: 'Peito',
-            ),
-            CardTreino(
-              nomeUsuario: 'Maria Fernanda',
-              dataTreino: 'Realizado em: 06/10 - Sábado',
-              Modalidade: 'Borboleta',
-            ),
-            CardTreino(
-              nomeUsuario: 'Pedro Souza',
-              dataTreino: 'Realizado em: 07/10 - Domingo',
-              Modalidade: 'Madley',
-            ),
-            CardTreino(
-              nomeUsuario: 'Mariana Oliveira',
-              dataTreino: 'Realizado em: 08/10 - Segunda Feira',
-              Modalidade: 'Borboleta',
-            ),
-            CardTreino(
-              nomeUsuario: 'Rafael Santos',
-              dataTreino: 'Realizado em: 09/10 - Terça Feira',
-              Modalidade: 'Peito',
-            ),
-            CardTreino(
-              nomeUsuario: 'Carla Mendes',
-              dataTreino: 'Realizado em: 10/10 - Quarta Feira',
-              Modalidade: 'Craw',
-            ),
-            CardTreino(
-              nomeUsuario: 'Fernando Rocha',
-              dataTreino: 'Realizado em: 11/10 - Quinta Feira',
-              Modalidade: 'Peito',
-            ),
-            CardTreino(
-              nomeUsuario: 'Isabela Lima',
-              dataTreino: 'Realizado em: 12/10 - Sexta Feira',
-              Modalidade: 'Costas',
-            ),
-          ],
-        ),
+          ),
+          Wrap(
+            children: [
+              FilterChip(
+                label: Text('Craw'),
+                selected: filters.contains(ExerciseFilter.Craw),
+                onSelected: (selected) {
+                  setState(() {
+                    if (selected) {
+                      filters.add(ExerciseFilter.Craw);
+                    } else {
+                      filters.remove(ExerciseFilter.Craw);
+                    }
+                  });
+                },
+              ),
+              // Adicione mais filtros aqui
+            ],
+          ),
+          CardTreino(
+            nomeUsuario: 'Gabriel Lamarca Galdino da Silva',
+            dataTreino: 'Realizado em: 03/10 - Quarta Feira',
+            Modalidade: 'Craw',
+          ),
+          CardTreino(
+            nomeUsuario: 'Ana Maria',
+            dataTreino: 'Realizado em: 04/10 - Quinta Feira',
+            Modalidade: 'Costas',
+          ),
+          CardTreino(
+            nomeUsuario: 'João da Silva',
+            dataTreino: 'Realizado em: 05/10 - Sexta Feira',
+            Modalidade: 'Peito',
+          ),
+          CardTreino(
+            nomeUsuario: 'Maria Fernanda',
+            dataTreino: 'Realizado em: 06/10 - Sábado',
+            Modalidade: 'Borboleta',
+          ),
+          CardTreino(
+            nomeUsuario: 'Pedro Souza',
+            dataTreino: 'Realizado em: 07/10 - Domingo',
+            Modalidade: 'Madley',
+          ),
+          CardTreino(
+            nomeUsuario: 'Mariana Oliveira',
+            dataTreino: 'Realizado em: 08/10 - Segunda Feira',
+            Modalidade: 'Borboleta',
+          ),
+          CardTreino(
+            nomeUsuario: 'Rafael Santos',
+            dataTreino: 'Realizado em: 09/10 - Terça Feira',
+            Modalidade: 'Peito',
+          ),
+          CardTreino(
+            nomeUsuario: 'Carla Mendes',
+            dataTreino: 'Realizado em: 10/10 - Quarta Feira',
+            Modalidade: 'Craw',
+          ),
+          CardTreino(
+            nomeUsuario: 'Fernando Rocha',
+            dataTreino: 'Realizado em: 11/10 - Quinta Feira',
+            Modalidade: 'Peito',
+          ),
+          CardTreino(
+            nomeUsuario: 'Isabela Lima',
+            dataTreino: 'Realizado em: 12/10 - Sexta Feira',
+            Modalidade: 'Costas',
+          ),
+        ],
       ),
     );
+  }
+
+  void buscarAtleta(String value) {
+    print('valor digitado: $value');
   }
 }
